80c01abad681353f65cda2e83ec0b87a23b15234
Matt Menzenski <matt.menzenski@gmail.com>
2017-04-10 17:48:20 -0500

    add type hint to return of get-all formats fn

--- src/mtg_sdk/client/format.clj
+++ src/mtg_sdk/client/format.clj
@@ -1,10 +1,13 @@
 (ns mtg-sdk.client.format
   (:require [clj-http.client :as client]
+            [schema.core :as s]
             [mtg-sdk.config :refer [base-url]]
+            [mtg-sdk.models :refer [Format]]
             [mtg-sdk.util :refer [parse-data]]))
 
 (def formats-url
   (format "%s/formats" base-url))
 
-(defn get-all []
+(s/defn get-all :- [[Format]]
+  []
   (-> formats-url client/get :body parse-data :formats))
