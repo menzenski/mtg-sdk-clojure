5927a3d56054f185ae3ca24283815602720ab7a8
Matt Menzenski <matt@mobilgov.com>
2017-04-22 21:56:15 -0500

    default 'lein deploy' to clojars

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [org.clojure/data.json "0.2.6"]
                  [prismatic/schema "1.1.5"]
                  [clj-http "3.4.1"]]
+  :deploy-repositories [["releases" :clojars]]
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"
             :distribution :manual})
