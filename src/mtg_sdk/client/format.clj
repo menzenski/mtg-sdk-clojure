3372a6b1c6097346a97bca7e7e071bf8cba04ad2
Matt Menzenski <matt@mobilgov.com>
2017-04-23 12:49:20 -0500

    tidy up some :require calls

--- src/mtg_sdk/client/format.clj
+++ src/mtg_sdk/client/format.clj
@@ -1,9 +1,9 @@
 (ns mtg-sdk.client.format
   (:require [clj-http.client :as client]
             [schema.core :as s]
-            [mtg-sdk.config :refer [base-url]]
-            [mtg-sdk.models :refer [Format]]
-            [mtg-sdk.util :refer [parse-data]]))
+            [mtg-sdk [config :refer [base-url]]
+                     [models :refer [Format]]
+                     [util :refer [parse-data]]]))
 
 (def formats-url
   (format "%s/formats" base-url))
